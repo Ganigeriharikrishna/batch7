cc42f09cdbd5568b2ad5305b0b1a96a5a2e423da
Ganigeriharikrishna <162981011+Ganigeriharikrishna@users.noreply.github.com>
2024-03-11 15:58:19 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,15 @@
+'''
+print("hello world")
+
+--->commenting
+1.)commenting is used for making documentation in a python file
+2.)when the line is commented,that line will not be considered as a part of the code.
+3.)when the program is executed,commented line will not be in the part of excution.
+
+--->NOTE:
+single line commenting can be done
+
+#print("hello world")
+'''
+import this
+print(this)
